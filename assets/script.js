c21f3bb2f6cc314e3b4de56de29fa128fd9ce240
Mikeysoar <mwclimb@gmail.com>
2021-05-12 17:51:16 -0600

    array selection

--- assets/script.js
+++ assets/script.js
@@ -19,6 +19,29 @@ function generatePassword() {
   if  (passwordLength < 8 || passwordLength > 30) {
     return 'Invalid password length!';
   }
+  else {
+    var userChoicesUpper = confirm('Do you want upper case letters?');
+    var userChoicesLower = confirm('Do you want lower case letters?');
+    var userChoicesNumbers = confirm('Do you want numbers letters?');
+    var userChoicesSymbols = confirm('Do you want symbols letters?');
+    if (!userChoicesUpper && !userChoicesLower && !userChoicesNumbers && !userChoicesSymbols) {
+      return 'Choose an option.';
+    } else {
+      if (userChoicesUpper) {
+        userSelection += upperCaseLetters
+      }
+      if (userChoicesLower) {
+        userSelection += lowerCaseLetters
+      }
+      if (userChoicesNumbers) {
+        userSelection += numbers
+      }
+      if (userChoicesSymbols) {
+        userSelection += symbols
+      }
+      console.log(userSelection)
+    }
+  }
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
